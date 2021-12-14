1c6e62ac037a2eea04123cd46aaf18958c87e9c1
tinh123ok <tinh123ok@gmail.com>
2021-12-14 08:12:28 +0700

    nhom3

--- Admin/header.php
+++ Admin/header.php
@@ -45,6 +45,7 @@ function menu_open($sp)
   <!-- Daterange picker -->
   <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
